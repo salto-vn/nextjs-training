ef86d09b7f2c7b270edce4f8add88b09a485733b
thuan.khuu <thuan.khuu@salto.vn>
2022-11-03 17:34:24 +0700

    Sumary: ENV, Font, next/image, next/router, next/link

--- thuan-nextjs/pages/_app.tsx
+++ thuan-nextjs/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/layout'
 import "bootstrap/dist/css/bootstrap.min.css"; 
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <Layout>
--- thuan-nextjs/pages/index.tsx
+++ thuan-nextjs/pages/index.tsx
@@ -1,6 +1,8 @@
 import styles from '../styles/Home.module.scss'
+import { Inter } from '@next/font/google'
 
 export default function Home() {
+
   return (
    <div className='container'>
     <h1>HOME</h1>
