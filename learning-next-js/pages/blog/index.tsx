683cfc501c3d697b1f3268d593cca564fa3d0f59
Anh Pham D <anh.pham@salto.vn>
2022-11-14 17:34:49 +0700

    Redirect and dynamic routes

---
+++ learning-next-js/pages/blog/index.tsx
@@ -0,0 +1,10 @@
+export default function () {
+    return (
+        <>
+        <div>
+            <h1>Blog</h1>
+            <button className='btn btn-primary'>Back</button>
+        </div>
+        </>
+    )
+}
---
+++ learning-next-js/pages/post/[id]/demo.tsx
@@ -0,0 +1,10 @@
+export default function () {
+    return (
+        <>
+        <div>
+            <h1>Demo</h1>
+            <button className='btn btn-primary'>Back</button>
+        </div>
+        </>
+    )
+}
