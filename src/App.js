bde79ed8d9f0a36bf0b470ba7440891a74ca2bdf
Anh Pham D <anh.pham@salto.vn>
2022-11-25 16:00:11 +0700

    Create page base on router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+import { Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Demo from "components/Demo";
 import Todo from "components/DucAnh/Todo";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Demo />
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <Demo /> */}
         <div className="duc-anh-page">
-          <Todo />
+          {/* <Todo /> */}
+
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="news">News</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </div>
       </header>
     </div>
