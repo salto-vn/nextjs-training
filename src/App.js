cd74ebbf106b25926738d0c5dbfb07a6fe388f25
trung.bui.saltovn <trung.bui@salto.vn>
2022-11-28 17:47:21 +0700

    Summary: create default layout.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { DefaultLayout } from "components/Layout";
+import { DefaultLayout } from "components/Layouts";
 import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { privateRoutes, publicRoutes } from "routes";
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,13 @@
+import styles from "./Login.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
 function Login() {
   return (
-    <>
+    <div className={cx("wrapper")}>
       <h1>Login Page</h1>
-    </>
+    </div>
   );
 }
 
