3ca6ac62bf12fb8fdf87dc6e2dc6015a1ea7eea3
Anh Pham D <anh.pham@salto.vn>
2022-11-28 11:32:01 +0700

    Add sass

--- src/components/DucAnh/Button/index.js
+++ src/components/DucAnh/Button/index.js
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import styles from "./Button.module.css";
+import styles from "./Button.module.scss";
 
 export default function Button(props) {
   const { primary } = props;
