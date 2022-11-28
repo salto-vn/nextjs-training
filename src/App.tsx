647375c1a30eeb327b74ee420496926f0b06999c
Anh Pham D <anh.pham@salto.vn>
2022-11-28 17:54:02 +0700

    Implement UI login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Users from "./pages/Users/Users";
+
 function App() {
   return (
     <div className="App">
-      REACT Typescript
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import LockIcon from "@mui/icons-material/Lock";
+import TextField from "@mui/material/TextField";
+import styles from "../../assets/scss/pages/_login.module.scss";
+
+export default function Login() {
+  return (
+    <div className={clsx(styles.loginPage)}>
+      <div className={clsx(styles.pageContent)}>
+        <div>
+          <LockIcon />
+          <h3>Sign in</h3>
+        </div>
+        <div className={clsx(styles.loginForm)}>
+          <TextField
+            className={clsx(styles.emailInput)}
+            required
+            id="outlined-required"
+            label="Email Address"
+            defaultValue="admin@gmail.com"
+          />
+          <TextField
+            className={clsx(styles.passwordInput)}
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
