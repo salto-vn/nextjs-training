f27be69dec28ef76088c3e67092c0fb30b22ee70
Anh Pham D <anh.pham@salto.vn>
2022-11-28 10:39:22 +0700

    Multiple class name

--- src/components/DucAnh/Paragraph/index.js
+++ src/components/DucAnh/Paragraph/index.js
@@ -1,5 +1,5 @@
 import styles from "./Paragraph.module.css";
 
 export default function Paragraph() {
-  return <h1 className={styles.paragraph}>Paragraph</h1>;
+  return <h1 className={`${styles.paragraph} ${styles.active}`}>Paragraph</h1>;
 }
